1c45e16fc12592946f9d6950f12aa4a81edc2b2d
xijiao <xijiaosdq@gmail.com>
2021-11-20 16:39:33 +0900

    enviroment config for iOS and android.

--- config.ts
+++ config.ts
@@ -1,7 +1,8 @@
+import { REACT_APP_BACKEND_BASE_URL } from '@env';
 
-var BACKEND_BASE_URL=process.env.REACT_APP_BACKEND_BASE_URL
-if (typeof BACKEND_BASE_URL === "undefined") {
-  BACKEND_BASE_URL = "http://192.168.50.40:8080";
+var BACKEND_BASE_URL=REACT_APP_BACKEND_BASE_URL
+if (typeof BACKEND_BASE_URL === "undefined" || BACKEND_BASE_URL === null) {
+  BACKEND_BASE_URL = "http://localhost:8080";
 }
 
 export { BACKEND_BASE_URL };
